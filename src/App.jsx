6efb94fd7f3cce646f922c480501ab207545568c
Nour Amged <nouramgged2005@gmail.com>
2025-07-14 15:49:41 +0300

    add initial implementation of CV application with styling and main components

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import SaveResume from "./components/saveCV";
+
+function Header() {
+  return <h1>CV Generator</h1>;
+}
+
+function App() {
+  return (
+    <>
+      <Header />
+      <SaveResume />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/saveCV.jsx
@@ -0,0 +1,12 @@
+function SaveResume() {
+  return (
+    <div className="cv-download-container">
+      <div className="cv-download">
+        <p>Save your resume</p>
+        <button className="download-btn">Download</button>
+      </div>
+    </div>
+  );
+}
+
+export default SaveResume;

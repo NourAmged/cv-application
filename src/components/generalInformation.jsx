9304c67c5ac59c1bb2e4efc9d70868d03ab78ae0
Nour Amged <nouramgged2005@gmail.com>
2025-07-15 10:59:31 +0300

    added opening and closing general information button

--- src/components/generalInformation.jsx
+++ src/components/generalInformation.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import downArrow from "../assets/down-arrow-5-svgrepo-com.svg";
 import person from "../assets/person-svgrepo-com.svg";
 import GiForm from "./generalInformationForm";
 
-
 function GeneralInformation() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="general-information-container">
       <div className="general-information">
@@ -12,9 +14,9 @@ function GeneralInformation() {
             <img src={person} width={32} />
             <p>General Information</p>
           </div>
-          <img src={downArrow} width={32} />
+          <img id="open-form" src={downArrow} width={32} style={{ cursor: "pointer" }} onClick={() => setIsOpen(!isOpen)}/>
         </div>
-        <GiForm />
+        {isOpen && <GiForm />}
       </div>
     </div>
   );

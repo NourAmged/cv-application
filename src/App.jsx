92d14be495b64e48d766859263a9044a217f457d
Nour Amged <nouramgged2005@gmail.com>
2025-07-16 14:07:53 +0300

    started working on cv generation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import GeneralInformation from "./components/generalInformation";
 import SaveResume from "./components/saveCV";
 import EducationExperience from "./components/educationExperience";
 import ProfessionalExperience from "./components/professionalExperience";
+import CV from "./components/cv";
 
 function Header() {
   return <h1>CV Generator</h1>;
@@ -11,11 +12,14 @@ function App() {
   return (
     <>
       <Header />
-      <div className="container">
-        <SaveResume />
-        <GeneralInformation />
-        <EducationExperience />
-        <ProfessionalExperience />
+      <div className="main-container">
+        <div className="container">
+          <SaveResume />
+          <GeneralInformation />
+          <EducationExperience />
+          <ProfessionalExperience />
+        </div>
+        <CV />
       </div>
     </>
   );
---
+++ src/components/cv.jsx
@@ -0,0 +1,9 @@
+function CV(){
+    return (
+        <div className="cv">
+
+        </div>
+    );
+}
+
+export default CV;

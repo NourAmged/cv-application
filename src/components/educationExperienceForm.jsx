60083792df74b868c473a9bdd2df9336d7d61f5b
Nour Amged <nouramgged2005@gmail.com>
2025-07-20 15:40:47 +0300

    started working on editing education experience

--- src/components/educationExperienceForm.jsx
+++ src/components/educationExperienceForm.jsx
@@ -1,4 +1,16 @@
-
+function EducationItems({ educationInfo }) {
+  return (
+    <div className="education-item">
+      <p>{educationInfo.degree}</p>
+      <p>{educationInfo.school}</p>
+      <p>{educationInfo.city}</p>
+      <p>{educationInfo.country}</p>
+      <p>
+        {educationInfo.start} - {educationInfo.end}
+      </p>
+    </div>
+  );
+}
 
 function EdForm({ onSubmitData, educationInfo }) {
   const handleSubmit = (e) => {
@@ -18,43 +30,50 @@ function EdForm({ onSubmitData, educationInfo }) {
   };
 
   return (
-    <form className="ed-form" onSubmit={handleSubmit}>
-      <label htmlFor="degree">Degree</label>
-      <input
-        id="degree"
-        name="degree"
-        placeholder="Enter Degree / Field of Study"
-        required
-      />
-      <br />
-      <label htmlFor="school">School</label>
-      <input
-        id="school"
-        name="school"
-        placeholder="Enter school / university"
-        required
-      />
-      <br />
-      <label htmlFor="city">City</label>
-      <input id="city" name="city" placeholder="Enter City" required />
-      <br />
-      <label htmlFor="country">Country</label>
-      <input id="country" name="country" placeholder="Country" required />
-      <br />
-      <div className="date-edu">
-        <label htmlFor="start">
-          Start Date
-          <br />
-          <input id="start" name="start" type="date" />
-        </label>
-        <label htmlFor="end">
-          End Date
-          <br />
-          <input id="end" name="end" type="date" />
-        </label>
-      </div>
-      <button type="submit">Save</button>
-    </form>
+    <>
+      <form className="ed-form" onSubmit={handleSubmit}>
+        <label htmlFor="degree">Degree</label>
+        <input
+          id="degree"
+          name="degree"
+          placeholder="Enter Degree / Field of Study"
+          required
+        />
+        <br />
+        <label htmlFor="school">School</label>
+        <input
+          id="school"
+          name="school"
+          placeholder="Enter school / university"
+          required
+        />
+        <br />
+        <label htmlFor="city">City</label>
+        <input id="city" name="city" placeholder="Enter City" required />
+        <br />
+        <label htmlFor="country">Country</label>
+        <input id="country" name="country" placeholder="Country" required />
+        <br />
+        <div className="date-edu">
+          <label htmlFor="start">
+            Start Date
+            <br />
+            <input id="start" name="start" type="date" />
+          </label>
+          <label htmlFor="end">
+            End Date
+            <br />
+            <input id="end" name="end" type="date" />
+          </label>
+        </div>
+        <button type="submit">Save</button>
+      </form>
+      {educationInfo.length === 0
+        ? ""
+        : educationInfo.map((edu, index) => {
+            return <EducationItems key={index} educationInfo={edu} />;
+          })}
+    </>
   );
 }
 
